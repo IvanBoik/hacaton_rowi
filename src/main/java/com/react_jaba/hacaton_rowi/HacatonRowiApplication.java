52b812e4cf672400ddf058f0fee0dc83ccd80043
IvanBoik <boiko12332@gmail.com>
2023-05-23 09:43:39 +0400

    Initial commit

---
+++ src/main/java/com/react_jaba/hacaton_rowi/HacatonRowiApplication.java
@@ -0,0 +1,13 @@
+package com.react_jaba.hacaton_rowi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacatonRowiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HacatonRowiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/react_jaba/hacaton_rowi/HacatonRowiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.react_jaba.hacaton_rowi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacatonRowiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
